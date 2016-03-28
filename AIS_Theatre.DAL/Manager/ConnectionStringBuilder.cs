096f940fdf06a073d78ae6256c1bd6bc2df7a80e
KachanAlexey <alexkachan@mail.ru>
2016-03-28 20:49:59 +0300

    Adding database managers

---
+++ AIS_Theatre.DAL/Manager/ConnectionStringBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIS_Theatre.DAL
+{
+    internal class ConnectionStringBuilder
+    {
+        private string _dbms;
+
+        internal ConnectionStringBuilder(string dbms)
+        {
+            this._dbms = dbms;
+        }
+
+        internal static string getConnectionString(string dbms)
+        {
+            switch (dbms)
+            {
+                case "PostgreSQL":
+                    PostgreSQLConnectionStringBuilder postgreSQLConnectionStringBuilder = new PostgreSQLConnectionStringBuilder();
+                    return postgreSQLConnectionStringBuilder.getConnectionString();
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ AIS_Theatre.DAL/Manager/PostgreSQLConnectionStringBuilder.cs
@@ -0,0 +1,37 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIS_Theatre.DAL
+{
+    internal class PostgreSQLConnectionStringBuilder
+    {
+        private string _server = "localhost";
+        private string _port = "5432";
+        private string _user = "postgres";
+        private string _password = "Masterkey";
+        private string _database = "theatre_db";
+
+        private string _connectionString = "";
+
+        private NpgsqlConnectionStringBuilder postrgreSQLStringBuilder = new NpgsqlConnectionStringBuilder();
+
+        internal PostgreSQLConnectionStringBuilder()
+        {
+            postrgreSQLStringBuilder.Add("Server", _server);
+            postrgreSQLStringBuilder.Add("Port", _port);
+            postrgreSQLStringBuilder.Add("User Id", _user);
+            postrgreSQLStringBuilder.Add("Password", _password);
+            postrgreSQLStringBuilder.Add("Database", _database);
+        }
+
+        internal string getConnectionString()
+        {
+            _connectionString = postrgreSQLStringBuilder.ConnectionString;
+            return _connectionString;
+        }
+    }
+}
