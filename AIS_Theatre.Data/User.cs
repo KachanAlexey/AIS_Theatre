5d1fb961b5d7025dc5638b8a410d8c5c8683e79f
KachanAlexey <alexkachan@mail.ru>
2016-04-25 23:14:14 +0300

    Adding User entity

---
+++ AIS_Theatre.Data/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIS_Theatre.Data
+{
+    public class User : BaseEntity<Guid>
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public User(string login, string password)
+        {
+            this.Id = Guid.NewGuid();
+            this.Login = login;
+            this.Password = password;
+        }
+
+        public User(Guid id, string login, string password)
+        {
+            this.Id = id;
+            this.Login = login;
+            this.Password = password;
+        }
+
+        public User Clone()
+        {
+            return (User)base.MemberwiseClone();
+        }
+
+        public override string ToString()
+        {
+            return Login;
+        }
+    }
