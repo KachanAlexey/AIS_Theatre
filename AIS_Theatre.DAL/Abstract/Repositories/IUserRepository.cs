842c2a3c0d03f2aaa062e02d3e9c8a48c09b2beb
KachanAlexey <alexkachan@mail.ru>
2016-04-25 23:14:27 +0300

    Adding user repository

---
+++ AIS_Theatre.DAL/Abstract/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using AIS_Theatre.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIS_Theatre.DAL
+{
+    public interface IUserRepository : IBaseRepository<Guid, User>
+    {
+    }
+}
---
+++ AIS_Theatre.DAL/PostgreSQL/Repositories/UserRepository.cs
@@ -0,0 +1,81 @@
+﻿using AIS_Theatre.Data;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIS_Theatre.DAL
+{
+    internal class UserRepository : BaseRepository<Guid, User>, IUserRepository
+    {
+        public UserRepository(NpgsqlConnection connection, NpgsqlTransaction transaction) : base(connection, transaction) { }
+
+        public override int Insert(User entity)
+        {
+            return (int)
+                base.ExecuteNonQuery(
+                        "insert into user_account (id_user, login_user, password_user) values (@Id, @Login, @Password)",
+                        new SqlParameters
+                        {
+                            {"Id", entity.Id.ToString()},
+                            {"Login", entity.Login},
+                            {"Passowrd", entity.Password},
+                        }
+                    );
+
+        }
+
+        public override bool Update(User entity)
+        {
+            var res = base.ExecuteNonQuery(
+                    "update user_account set login_user = @Name, password_user = @Password where id_user = @Id ",
+                    new SqlParameters
+                    {
+                            {"Id", entity.Id.ToString()},
+                            {"Login", entity.Login},
+                            {"Passowrd", entity.Password},
+                    }
+                );
+
+            return res > 0;
+        }
+
+        public User GetById(Guid id)
+        {
+            return base.ExecuteSingleRowSelect(
+                    "select * from user_account where id_user = @Id",
+                    new SqlParameters()
+                    {
+                        {"Id", id.ToString()}
+                    }
+                );
+        }
+
+        public bool Delete(Guid id)
+        {
+            var res = base.ExecuteNonQuery(
+                "delete from user_account where id_user = @Id",
+                new SqlParameters()
+                {
+                    { "Id", id.ToString() }
+                });
+
+            if (res > 1)
+                throw new InvalidOperationException("Multiple rows deleted by single delete query");
+
+            return res == 1;
+        }
+
+        public List<User> GetAll()
+        {
+            return base.ExecuteSelect("select * from user");
+        }
+
+        protected override User DefaultRowMapping(NpgsqlDataReader reader)
+        {
+            return new User(Guid.Parse((string)reader["id_user"]), (string)reader["login_user"], (string)reader["password_user"]);
+        }
+    }
+}
